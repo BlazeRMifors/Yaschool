90e0ff8d654586086f9a5ce49ff90b8b6d7d3f23
Isaev Ivan <blazer@mifors.com>
2025-06-13 12:44:02 +0400

    Initial Commit

---
+++ Yaschool/YaschoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaschoolApp.swift
+//  Yaschool
+//
+//  Created by Ivan Isaev on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct YaschoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
